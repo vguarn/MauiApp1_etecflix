00dc23de0ae5f663d9928753045f7b5704cde97f
Usuario <Usuario@CPD03-M15>
2024-04-29 13:53:48 -0300

    Adicionar arquivos de projeto.

---
+++ MauiApp1_etecflix/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApp1_etecflix
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
